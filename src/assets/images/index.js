e4b7e803aa5139bec7a3cce6ce84b79324acc641
Jacky Lam <jackykaholam@hotmail.com>
2022-04-17 01:22:08 +0800

    [Home Screen] init screen

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,5 +1,16 @@
+import CreateNoteIcon from '~src/assets/images/icons/create-note-icon.svg';
+import CreateTaskIcon from '~src/assets/images/icons/create-task-icon.svg';
 import NavNoteIcon from '~src/assets/images/icons/nav-note-icon.svg';
 import NavPlusIcon from '~src/assets/images/icons/nav-plus-button.svg';
 import NavTaskIcon from '~src/assets/images/icons/nav-task-icon.svg';
+import PlusIcon from '~src/assets/images/icons/plus-icon.svg';
 import SettingIcon from '~src/assets/images/icons/setting-icon.svg';
-export { NavPlusIcon, NavNoteIcon, NavTaskIcon, SettingIcon };
+export {
+  NavPlusIcon,
+  NavNoteIcon,
+  NavTaskIcon,
+  SettingIcon,
+  CreateTaskIcon,
+  CreateNoteIcon,
+  PlusIcon,
+};
--- src/screens/Common/HomeScreen.js
+++ src/screens/Common/HomeScreen.js
@@ -1,20 +1,52 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import { ScrollView, StyleSheet, Text, View } from 'react-native';
 
+import BaseHeader from '../../components/BaseHeader';
 import useLocalization from '~src/contexts/i18n';
 import { AppDefaultTheme } from '~src/contexts/theme/AppTheme';
 import { Typography } from '~src/styles';
 import { sw } from '~src/styles/Mixins';
+import {
+  CreateNoteIcon,
+  CreateTaskIcon,
+  PlusIcon,
+  SettingIcon,
+} from '../../assets/images';
 
 const HomeScreen = ({ navigation }) => {
   const { t, locale, setLocale } = useLocalization();
   const theme = AppDefaultTheme.settings;
   const styles = getStyle(theme);
 
+  useEffect(() => {
+    console.log('HomeScreen');
+  }, []);
+
+  const renderCreateItemView = (title, icon, callback) => {
+    return (
+      <View style={styles.itemView}>
+        {icon}
+        <View style={styles.btnView}>
+          <View style={styles.plusIconView}>
+            <PlusIcon />
+          </View>
+          <Text style={styles.btnTitleText}>{title}</Text>
+        </View>
+      </View>
+    );
+  };
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Home Screen</Text>
+    <View style={{ flex: 1 }}>
+      <BaseHeader notShowBackIcon={true} />
+      <View style={styles.container}>
+        <View style={styles.settingView}>
+          <SettingIcon />
+        </View>
+        <Text style={styles.text}>Note Space</Text>
+        {renderCreateItemView('Create Task', <CreateTaskIcon />)}
+        {renderCreateItemView('Create Note', <CreateNoteIcon />)}
+      </View>
     </View>
   );
 };
@@ -23,12 +55,56 @@ const getStyle = (theme) => {
   return StyleSheet.create({
     container: {
       flex: 1,
+      backgroundColor: '#1B191E',
       alignItems: 'center',
-      justifyContent: 'center',
     },
     text: {
       ...Typography.ts(theme.fonts.weight.bold, sw(45)),
-      color: '#000',
+      color: '#FFF',
+      textAlign: 'center',
+    },
+    settingView: {
+      marginTop: sw(12),
+      marginRight: sw(40),
+      marginBottom: sw(20),
+      alignSelf: 'flex-end',
+    },
+    itemView: {
+      width: sw(332),
+      backgroundColor: '#29282D',
+      alignItems: 'center',
+      borderRadius: sw(20),
+      paddingTop: sw(26),
+      marginVertical: sw(28),
+      shadowOpacity: 0.5,
+      shadowRadius: 30,
+      shadowOffset: { 0: 10 },
+      shadowColor: '#000',
+      elevation: 10,
+    },
+    btnView: {
+      width: '100%',
+      flexDirection: 'row',
+      alignItems: 'center',
+      backgroundColor: '#424450',
+      borderRadius: sw(20),
+      justifyContent: 'center',
+      paddingVertical: sw(8),
+      marginTop: sw(18),
+    },
+    plusIconView: {
+      shadowOpacity: 0.5,
+      shadowRadius: 6,
+      shadowOffset: { 0: 10 },
+      shadowColor: '#000',
+      elevation: 10,
+      alignSelf: 'center',
+      justifyContent: 'center',
+    },
+    btnTitleText: {
+      ...Typography.ts(theme.fonts.weight.bold, sw(20)),
+      color: '#FFF',
+      paddingLeft: sw(18),
     },
   });
 };
